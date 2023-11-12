c3ece8ff17a713097864847ee331b02fe995c806
rashi-ash <83757768+rashi-ash@users.noreply.github.com>
2023-11-13 00:57:30 +0530

    Update settings.py

--- Stackup/settings.py
+++ Stackup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-v@xym$+b55=9(mngoh(gtw9!49#jd!)ocbdg)1bk6-tva8-(%y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
