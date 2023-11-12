a56d056be97ee53690fa4893e021984ef32ea15d
rashi-ash <rashidav27@gmail.com>
2023-11-12 19:13:37 +0530

    venvactivate

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-# Build the project
+# #!/bin/bash
+# # Build the project
 
-echo "Building the project..."
-python3.9 -m install -r requirements.txt
+# echo "Building the project..."
+# python3.9 -m install -r requirements.txt
 
-echo "Make Migrations.."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+# echo "Make Migrations.."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
 
-echo "Collect Static.."
-python3.9 manage.py collectstatic --noinput --clear
+# echo "Collect Static.."
+# python3.9 manage.py collectstatic --noinput --clear
